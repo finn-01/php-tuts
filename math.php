49d2e630cfbbb88f96b00c83b3dcba8f902187a2
finn-01 <nguyensam576@gmail.com>
2022-03-07 23:17:02 +0700

    math.php

---
+++ math.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Math</title>
+</head>
+<body>
+<?php
+//pi(), min() and max() abs() sqrt() round() rand()
+//https://www.w3schools.com/php/php_ref_math.asp
+?>
+</body>
+</html>
