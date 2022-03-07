088cc30da0c81ca375ffc32d71473d7a01b89116
finn-01 <nguyensam576@gmail.com>
2022-03-07 22:47:41 +0700

    strings.php

---
+++ strings.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Strings</title>
+</head>
+<body>
+<?php
+//strlen(): Hàm strlen () trong PHP trả về độ dài của một chuỗi.
+echo strlen("Hello World!");
+echo "<br>";
+
+//str_word_count(): hàm đếm số từ trong một chuỗi.
+echo str_word_count("Hello World! LOL");
+echo "<br>";
+
+//strrev() - Reverse a String
+echo strrev("Hello world!"); // outputs !dlrow olleH
+echo "<br>";
+
+//strpos() tim kiem van ban
+echo strpos("Hello world!", "world"); // outputs 6
+
+//str_replace()
+echo "<br>";
+echo str_replace("world", "Dolly", "Hello world!"); // outputs Hello Dolly!
+
+?>
+</body>
+</html>
