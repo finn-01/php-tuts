2111fad45710faa3c3b4ef05e30135dc950d132c
finn-01 <nguyensam576@gmail.com>
2022-03-07 23:52:44 +0700

    ifelse

---
+++ ifelse.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>If else</title>
+</head>
+<body>
+
+<!-- if statement - executes some code if one condition is true
+if...else statement - executes some code if a condition is true and another code if that condition is false
+if...elseif...else statement - executes different codes for more than two conditions -->
+
+<?php
+$t = date("H");
+echo "<p>The hour (of the server) is " . $t; 
+echo ", and will give the following message:</p>";
+
+if ($t < "10") {
+  echo "Have a good morning!";
+} elseif ($t < "20") {
+  echo "Have a good day!";
+} else {
+  echo "Have a good night!";
+}
+?>
+
+</body>
+</html>
