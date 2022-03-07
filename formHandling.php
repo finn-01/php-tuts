a8b44ee6711f3f8f78532df691c3ce645762a2a8
finn-01 <nguyensam576@gmail.com>
2022-03-08 00:47:33 +0700

    form

---
+++ formHandling.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Form Handle</title>
+</head>
+<body>
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+</body>
+</html>
