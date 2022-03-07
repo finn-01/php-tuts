98b22bbe416a8d41da1080d5dbf3e07fa9ce9399
finn-01 <nguyensam576@gmail.com>
2022-03-07 23:49:08 +0700

    operators.php

---
+++ oparators.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Operator</title>
+</head>
+<body>
+<?php
+$txt1 = "Hello";
+$txt2 = " world!";
+//Concatenation
+echo $txt1 . $txt2;
+
+echo "<br>";
+
+//Appends 
+$txt1 .= $txt2;
+echo $txt1;
+?>  
+</body>
+</html>
