fa711f9dd0197007134c753fd49cc156fcf7566b
finn-01 <nguyensam576@gmail.com>
2022-03-12 01:43:18 +0700

    php_session2.php

---
+++ demo_session2.php
@@ -0,0 +1,18 @@
+<?php 
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Session</title>
+</head>
+<body>
+<?php 
+echo "Fav color is " . $_SESSION['favcolor'] . "<br>";
+echo "Fav animal is " . $_SESSION['favanimal'] . "<br>";
+?>
+</body>
+</html>
