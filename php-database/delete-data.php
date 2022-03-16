a9d1aa17ebad26cc90e7f41fe6ba9dd8559c34c3
finn-01 <nguyensam576@gmail.com>
2022-03-16 17:00:37 +0700

    delete data

---
+++ php-database/delete-data.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Delete</title>
+</head>
+<body>
+<?php 
+$servername = "127.0.0.1:3306";
+$username = "root";
+$password = "123456789";
+$dbname="myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to delete a record
+$sql = "DELETE FROM MyGuests WHERE id=3";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record deleted successfully";
+} else {
+  echo "Error deleting record: " . $conn->error;
+}
+
+$conn->close();
+?>
+</body>
+</html>
