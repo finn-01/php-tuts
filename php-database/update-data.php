e8371cfbf4b13ba01cc45aaa2750d3422a919c2e
finn-01 <nguyensam576@gmail.com>
2022-03-16 17:05:20 +0700

    update

---
+++ php-database/update-data.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Update</title>
+</head>
+<body>
+<?php 
+$servername = "127.0.0.1:3306";
+$username = "root";
+$password = "123456789";
+$dbname="myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "UPDATE MyGuests SET lastname='Doe1' WHERE id=2";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record updated successfully";
+} else {
+  echo "Error updating record: " . $conn->error;
+}
+
+$conn->close();
+?>
+</body>
+</html>
